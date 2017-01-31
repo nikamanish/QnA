6fc8f6ae5da493c1b5ff0a934c558f641b2c42e1
Idyllic <nikamanish007@gmail.com>
2017-01-31 17:27:34 +0530

    Test group validations

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -4,4 +4,26 @@ class GroupTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@group = Group.new(name: "Walchand")
+  end
+
+  test "name should be unique" do
+  	dup_group = @group.dup
+  	@group.save
+  	assert_not dup_group.valid?
+  end
+
+  test "name should be of minimum length 6" do
+  	@group.name = "a"*5
+  	@group.save
+  	assert_not @group.valid?
+  end
+
+  test "name should not be blank" do
+  	@group.name = "       "
+  	@group.save
+  	assert_not @group.valid?
+  end
 end
